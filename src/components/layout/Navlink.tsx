a431d7073e652ae40f859a5f04768592e90e5a4a
Alex <david_alejandrorizo@outlook.com>
2025-10-22 11:57:37 -0600

    sidebar animation bug fixed

--- src/components/layout/Navlink.tsx
+++ src/components/layout/Navlink.tsx
@@ -17,7 +17,7 @@ export const Navlink: FC<Props> = ({ to, icon, label, collapsed }) => {
       to={to}
       className={cn(
         'flex items-center gap-2 h-8.5 w-full overflow-hidden rounded hover:bg-violet-200 transition-colors px-2',
-        collapsed && 'justify-center px-0 gap-0',
+        collapsed && 'p-1.5 gap-0 size-min',
       )}
       activeProps={{ className: 'bg-violet-200' }}
       inactiveProps={{ className: 'bg-gray-100' }}
