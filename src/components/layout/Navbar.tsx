b6c988d28ca85fbb9004092697ffd0419d22b364
Alex <david_alejandrorizo@outlook.com>
2025-12-12 11:06:54 -0600

    users nav toggle

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,17 @@
+import { useLocation } from '@tanstack/react-router'
 import { BoardsNav } from './BoardsNav'
 import { Breadcrumbs } from './Breadcrumbs'
 import { UserNav } from './UserNav'
 
 export const Navbar = () => {
+  const { pathname } = useLocation()
+
   return (
     <nav className="w-full px-4 h-16 flex items-center justify-between bg-white border-b relative">
       <Breadcrumbs />
       <div className="flex items-center gap-2 absolute left-1/2 -translate-x-1/2">
         <BoardsNav />
-        <UserNav />
+        {pathname.includes('/solicitudes') && <UserNav />}
       </div>
       <div className="ml-auto"></div>
     </nav>
